bfd1d33080b23c4485ae9e7d53f526cc38f32d46
Minghao Wang <minghao.wang@thoughtworks.com>
2020-01-02 19:01:49 +0800

    change package to group3;

--- src/main/java/com/wuhantoc/javasample/enums/UnitEnum.java
+++
@@ -1,7 +0,0 @@
-package com.wuhantoc.javasample.enums;
-
-public enum  UnitEnum {
-  YARD,
-  FOOT,
-  INCH
-}
--- src/main/java/com/wuhantoc/javasample/group3/JavaSampleApplication.java
+++ src/main/java/com/wuhantoc/javasample/group3/JavaSampleApplication.java
@@ -1,4 +1,4 @@
-package com.wuhantoc.javasample;
+package com.wuhantoc.javasample.group3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
